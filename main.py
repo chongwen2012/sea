395d78911c186599ee93173ee846780bf4b637b5
chongwen2012 <11086559+liangjian1099@user.noreply.gitee.com>
2023-04-09 12:43:34 +0800

    正常出海，代码需要优化

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from PIL import ImageGrab
 import cv2
 import numpy as np
 import pyautogui
-import pytesseract
+import time
+import pydirectinput
+
+
+
 
 def capture_window(window_title):
     # 获取窗口句柄
@@ -15,6 +19,9 @@ def capture_window(window_title):
     # 捕获窗口截图
     bbox = (left, top, right, bottom)
     print(bbox)
+    # 将窗口激活到前台
+    # win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)  # 还原窗口
+    # win32gui.SetForegroundWindow(hwnd)  # 将窗口置于顶层
     return bbox
     #image = ImageGrab.grab(bbox)
 
@@ -54,30 +61,45 @@ def calculate_center(x, y, w, h):
     return center_x, center_y
 
 
-def find_target_by_img(bbox):
-    # 读取屏幕截图和需要匹配的图片
-    screen = np.array(ImageGrab.grab(bbox))
-    template = cv2.imread("E:\python\sea\gkgr.png")
+def find_target_by_img(bbox,img,clickTimes=1,Deviation=0):    
+    location=pyautogui.locateCenterOnScreen(img,confidence=0.6,region=bbox)
+    if location is not None:
+        try:
+            pyautogui.click(location.x+Deviation,location.y+Deviation,clicks=clickTimes,interval=1,duration=0.2,button='left')   
+        except pyautogui.FailSafeException as e:
+            print(e) 
+        # pyautogui.moveTo(location.x, location.y)
+        # pydirectinput.mouseDown()
+        # time.sleep(0.5)
+        # pydirectinput.mouseUp()        
+    else:    
+        print("未找到匹配图片,0.1秒后重试")
+    # time.sleep(0.1) 
 
-    # 将需要匹配的图片转为灰度图像
-    gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-
-    # 使用模板匹配函数在屏幕截图中找到匹配的位置
-    res = cv2.matchTemplate(screen, gray, cv2.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    # 根据匹配结果确定目标位置
-    x, y = max_loc
-    w, h = template.shape[1], template.shape[0]
-    center_x, center_y = x + w//2, y + h//2
-    pyautogui.click(x=center_x,y=center_y)
 
 if __name__ == '__main__':
     window_title = "海之乐章-重启 V1.60.01A  (Build:Apr  4 2023,10:50:20)"
+    # window_title = "Spotify Premium"
     # 获取到窗口的位置
     bbox = capture_window(window_title)
     # 传入港口工人的颜色和窗口范围
     # lower_color=np.array([40,40,248])
     # upper_color=np.array([40,40,248])
     # find_target_area(bbox,lower_color,upper_color)   
-    find_target_by_img(bbox)
+    img='1.png'
+    find_target_by_img(bbox,img)
+    img2='2.png'
+    find_target_by_img(bbox,img2)
+    img3='3.png'
+    find_target_by_img(bbox,img3,4)
+    img4='4.png'
+    find_target_by_img(bbox,img4)
+    find_target_by_img(bbox,img)
+    img5='5.png'
+    find_target_by_img(bbox,img5)
+    img6='6.png'
+    time.sleep(12)
+    find_target_by_img(bbox,img6,Deviation=-70)
+    img7='7.png'
+    time.sleep(1)
+    find_target_by_img(bbox,img7)
